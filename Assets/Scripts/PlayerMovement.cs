60d4fcd3b6b0682a8fec7a6e3411bcfb3e009b65
CogNoman <cognoman@hotmail.com>
2026-05-22 23:14:47 -0400

    Add Input Controller to Skillet

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerInput _pi;
+    private InputAction _actMove;
+    private InputAction _actPause;
+    private Vector2 _movement = new();
+    private Rigidbody2D _rb;
+    const float MAX_SPEED = 10f;
+
+    void Awake()
+    {
+        _pi = GetComponentInParent<PlayerInput>();
+        _rb = GetComponent<Rigidbody2D>();
+        _actMove = _pi.actions["Move"];
+    }
+
+    void Update()
+    {
+        _movement = _actMove.ReadValue<Vector2>();
+        // Debug.Log(_actMove.ReadValue<Vector2>());
+        // if(_movement.x > 0)
+        // {
+        //     Debug.Log(_movement.x);
+        //     Debug.Log(_movement.y);
+        // }
+
+        if (_movement.x != 0 || _movement.y != 0)
+        {
+            _rb.AddForce(_movement.normalized * 5f, ForceMode2D.Force);
+            // _rb.MovePosition(_movement.normalized * 1f);
+            // _rb.linearVelocity = _movement.normalized * 1f;
+            // gameObject.transform.Translate(_movement * 8f * Time.deltaTime);
+            if(_rb.linearVelocity.magnitude > MAX_SPEED)
+            {
+                _rb.linearVelocity = _movement.normalized * MAX_SPEED;
+            }
+        }
+        else
+        {
+            _rb.AddForce(_rb.linearVelocity * -5f, ForceMode2D.Force);
+        }
+    }
+}
