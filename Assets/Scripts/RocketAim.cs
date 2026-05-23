be07e1128d1fb9b6a01ecbbc8b9348c30cf6d85a
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2026-05-23 10:47:20 -0400

    rocket

--- Assets/Scripts/RocketAim.cs
+++ Assets/Scripts/RocketAim.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class RocketAim : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    private Vector2 velocity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        velocity = _rb.linearVelocity;
+        if(velocity != Vector2.zero)
+        {
+            transform.up = velocity;
+        }
     }
 }
