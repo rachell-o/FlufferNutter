0d5c7c3479d23546fb7bcc4e1d9585c047de9770
apaquette-mobileapps <alexterieur6666@outlook.com>
2026-05-23 20:41:35 -0400

    Added Main Menu

--- Assets/Scripts/ButtonForCursor.cs
+++ Assets/Scripts/ButtonForCursor.cs
@@ -4,35 +4,27 @@ using UnityEngine.Events;
 
 public class ButtonForCursor : MonoBehaviour
 {
-    [SerializeField] AudioClip _clicSound;
     [SerializeField] UnityEvent _even;
     [SerializeField] Sprite _highlightedSprite;
     Sprite _normalSprite;
     SpriteRenderer _sr;
     // Image _img;
-
+    
     void Start()
     {
-        if (_highlightedSprite != null)
-        {
-            _sr = GetComponent<SpriteRenderer>();
-            _normalSprite = _sr.sprite;
-        }
+        _sr = GetComponent<SpriteRenderer>();
+        _normalSprite = _sr.sprite;
     }
 
     public void ChangeSpirte(bool highlight)
     {
-        if (_highlightedSprite != null)
-        {
-            if (highlight && _sr.sprite != _highlightedSprite) _sr.sprite = _highlightedSprite;
-            else if (!highlight && _sr.sprite == _highlightedSprite) _sr.sprite = _normalSprite;
-        }
+        if(highlight && _sr.sprite != _highlightedSprite) _sr.sprite = _highlightedSprite;
+        else if(!highlight && _sr.sprite == _highlightedSprite) _sr.sprite = _normalSprite;
     }
 
     public void TriggerEvent()
     {
         Debug.Log("Je suis Triggered!");
-        SoundManager.instance.PlaySound(_clicSound);
         _even.Invoke();
     }
 }
