36db76c3683185f5696e735758c8533cb42e793f
CogNoman <cognoman@hotmail.com>
2026-05-24 12:39:49 -0400

    Add GameManager to AsteroidGame

--- Assets/Scripts/Asteroid/RocketShoot.cs
+++ Assets/Scripts/Asteroid/RocketShoot.cs
@@ -16,7 +16,7 @@ public class RocketShoot : MonoBehaviour
 
     private void Awake()
     {
-        _pi = GetComponent<PlayerInput>();
+        _pi = GameManager.instance.Pi;
         actShoot = _pi.actions["Attack"];
     }
 
