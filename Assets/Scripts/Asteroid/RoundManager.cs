01f744bc883b2b47aaded5b150c3137edb3825ba
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2026-05-23 23:29:07 -0400

    add transition to final scene to astroid minigame

--- Assets/Scripts/Asteroid/RoundManager.cs
+++ Assets/Scripts/Asteroid/RoundManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RoundManager : MonoBehaviour
 {
     [SerializeField] GameObject failText;
     [SerializeField] GameObject winText;
+    [SerializeField] SpriteRenderer black;
+
+    Color color;
+
+    bool failed = false;
 
     public void Win()
     {
@@ -16,13 +22,48 @@ public class RoundManager : MonoBehaviour
         yield return new WaitForSeconds(12f);
 
         winText.SetActive(true);
+        StartCoroutine(GoodEnding());
+    }
+
+    IEnumerator GoodEnding()
+    {
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("SceneGEA");
     }
 
     public void Fail()
     {
         Time.timeScale = 0;
         failText.SetActive(true);
+
+        failed = true;
+
+        StartCoroutine(BadEnding());
     }
 
+    IEnumerator BadEnding()
+    {
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene("SceneBEA");
+    }
+
+    private void Update()
+    {
+        if (failed)
+        {
+            color = black.color;
+            color.a = Mathf.Lerp(color.a, 255f, 0.00001f);
+            black.color = color;
+
+            Debug.Log(black.color.a);
+            if (black.color.a >= 2.5)
+            {
+                Time.timeScale = 1;
+                StartCoroutine(BadEnding());
+            }
+        }
+    }
 
 }
