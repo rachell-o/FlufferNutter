42a4256de67f283fe8469d4bd289e7cace1bf3ba
apaquette-mobileapps <alexterieur6666@outlook.com>
2026-05-24 12:19:43 -0400

    Fix Sound

--- Assets/Scripts/Core/Managers/SoundManager.cs
+++ Assets/Scripts/Core/Managers/SoundManager.cs
@@ -26,7 +26,7 @@ public class SoundManager : MonoBehaviour
             return false; //échec!
         }
         _instance = this;
-        DontDestroyOnLoad(gameObject);
+        // DontDestroyOnLoad(gameObject);
         return true; //succès!
     }
 
